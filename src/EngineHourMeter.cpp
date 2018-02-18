e53d4c4f9cfd38207265c1dbf98aa2c0a01a93ea
mishagin-tim <timerhan999@gmail.com>
2018-02-18 23:23:02 +0300

    Add declarations for EngineHourMeter

---
+++ src/EngineHourMeter.cpp
@@ -0,0 +1,9 @@
+#include "EngineHourMeter.h"
+
+EngineHourMeter::EngineHourMeter() {
+
+}
+
+EngineHourMeter::~EngineHourMeter() {
+
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include "EngineHourMeter.h"
 
 void setup() {
     // put your setup code here, to run once:
